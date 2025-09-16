a1a7b8847f9c033e80f9ba9f9aacdba78a78c7d5
AleGoncharov <a.goncharov@tg.dunice.net>
2025-09-16 20:53:00 +0300

    fix t9

--- Practise2/part_1/9_additional_for_7.py
+++ Practise2/part_1/9_additional_for_7.py
@@ -4,4 +4,4 @@ print(variable[:6]) # Объект
 print(variable[9:17]) # Ориентир
 print(variable[14:17]) # Тир
 print(variable[4]+variable[7]+variable[5]) # Кот
-print(variable[10]+variable[12:15]+variable[::])
+print(variable[10]+variable[12:15]+variable[-5]) # Рента
